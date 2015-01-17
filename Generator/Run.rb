f6bfa05edaf5b951d8def0c87dc4c50864839769
Zbigniew Krolikowski <zbkrolikowski@gmail.com>
2015-01-17 16:27:55 +0100

    Praktycznie wszystko juz dziala, tylko konflikty i platnosci sa do zrobienia

--- Generator/Run.rb
+++ Generator/Run.rb
@@ -45,69 +45,123 @@ conferences.each{|x| ((NR_WOKSHOPS_PER_CON_VAR*rand()).to_i + NR_WOKSHOPS_PER_CO
 wCollisions = Hash.new
 workshops.each{|x| workshops.each{|y| wCollisions["#{x.id} #{y.id}"] = Workshop.Collide x, y }}
 
-# Making conference reservations
 
-creservations = Array.new
-conferences.each do |x| 
-	((NR_RESERVATIONS_PER_CON_FIRMS_VAR*rand()).to_i + NR_RESERVATIONS_PER_CON_FIRMS_BASE).times do
-		creservations << CReservation.new(clientFirms.sample, x.days.sample)
-	end
-end # This was supposed to be in one line but ran into some syntax bug :) 
-
-wreservations = Array.new
-workshops.each do |x| 
-	((NR_RESERVATIONS_PER_WORK_VAR*rand()).to_i + NR_RESERVATIONS_PER_WORK_BASE).times do
-		wreservations << WReservation.new(x, creservations.select{|cr| cr.conference == x.conference}.sample)
-	end
-end # Had to make a little query there
-
-#Now, the hardest part will be filling these conferences and workshops with people
 
 #Just to divide people into students and not students
 normal = people.select{|x| x.nr == nil}
 students = people.select{|x| x.nr != nil}
+# Array of conference reservations
+creservations = Array.new
+# Array of workshop reservations
+wreservations = Array.new
+
+# Filling the conferences with reservations, fisrly for companies
+conferences.each do |x|
+	((NR_RESERVATIONS_PER_CON_FIRMS_VAR*rand()).to_i + NR_RESERVATIONS_PER_CON_FIRMS_BASE).times do
+		creservations << CReservation.new(clientFirms.sample, x.days.sample)
+	end
+end
+
+# And now for individuals
+conferences.each do |x|
+	((NR_RESERVATIONS_PER_CON_FIRMS_VAR*rand()).to_i + NR_RESERVATIONS_PER_CON_FIRMS_BASE).times do
+		creservations << CReservation.new(clientInd.sample, x.days.sample)
+	end
+end 
+
+# Now filling the conference reservations with conference reservations
+workshops.each do |x| 
+	# Conference reservations for a given workshop
+	workConferencesRes = creservations.select{|cr| cr.conference == x.conference}
+	((NR_RESERVATIONS_PER_WORK_VAR*rand()).to_i + NR_RESERVATIONS_PER_WORK_BASE).times do
+		wreservations << WReservation.new(x, workConferencesRes.sample)
+		# Added a workshop reservation
+	end
+end # Filling the conferences with reservations
+
+# Array of conference participants
 cparticipants = Array.new
-creservations.each do |x|
-	x.normal.times do
-		cparticipants << (CParticipant.new x, normal.sample)
-	end
-	x.students.times do
-		cparticipants << (CParticipant.new x, students.sample)
-	end
-end
-
+# Array of workshop pariticipants
 wparticipants = Array.new
-wreservations.each do |x|
-		x.places.times{wparticipants << (WParticipant.new x, cparticipants.sample)}
-end
 
-#We have to do the whole thing differently just for ind customers
-#Making a support array just for that
-creservationsInd = Array.new
-conferences.each do |x| 
-	((NR_RESERVATIONS_PER_CON_IND_VAR*rand()).to_i + NR_RESERVATIONS_PER_CON_IND_BASE).times do
-		creservationsInd << CReservation.new(clientInd.sample, x.days.sample)
+# Now let's fill em up
+# We will will every conference reservation with cparticipants, and then it's conference reservations
+creservations.each do |x|
+	# We will do this for every conference reservation
+	# Firstly for individual reservations
+	# INDIVIDUAL
+	if x.client.instance_of?(IndClient) 
+		cparticipant = CParticipant.new x, x.client.person
+		cparticipants << cparticipant
+		# Now for every workshop reservation refering to this conference reservation we will add one participant to it's workshop
+		wreservations.select{|wres| wres.creservation = x}.each do |wres|
+			wparticipants << (WParticipant.new wres, x.client)
+			# Added a workshop participant for this workshop and for the client of this conference reservation
+		end
+		# For every workshop this client had a reservation, added him to the participants
+	else
+		# Here we got a company reservation
+		# COMPANY
+		# This are very important structures 
+		# they contating the people already chosen to fill in for this reservation
+		cParForThisConference = Array.new
+		# Choosing some normal guys, not a students, to participate
+		# AS FOR NOW THEY MAY HAVE SOME CONFLICTS WITH DIFFERENT CONFERENCES AND RESERVATIONS
+		x.normal.times do
+			guy = normal.sample # We choose a guy, not a student
+			thisParticpant = (CParticipant.new x, guy)
+			cparticipants << thisParticpant
+			# Added the guy to the list
+			cParForThisConference << thisParticpant
+		end
+		# Choosing some students, to participate
+		# AS FOR NOW THEY MAY HAVE SOME CONFLICTS WITH DIFFERENT CONFERENCES AND RESERVATIONS
+		x.students.times do
+			guy = students.sample # We choose a guy, not a sample
+			thisParticpant = (CParticipant.new x, guy)
+			cparticipants << thisParticpant
+			# Added the guy to the list
+			cParForThisConference << thisParticpant
+		end
+		# The same thing, only for workshop reservations, student don't matter to me none :D	
+		# the workshop participant structure doesn't differentiate from students
+		# AS FOR NOW THEY MAY HAVE SOME CONFLICTS WITH DIFFERENT CONFERENCES AND RESERVATIONS
+
+		# Now for every workshop reservation refering to this conference reservation we will add participants to it's workshop
+		# choosing only the wreservations for this conference reservation
+		wreservations.select{|wres| wres.creservation = x}.each do |wres|
+			wres.places.times do
+			wparticipants << (WParticipant.new wres, cParForThisConference.sample)
+			# Added a workshop participant for this workshop and for the client of this conference reservation
+			end
+		end
+		# We filled the whole stuff with people
+		# They may have some conflicts
 	end
 end
-creservations << creservationsInd # Adding ind conference reservations to big table
-
-## Addding individual conference participants to 
-creservationsInd.each do |x|
-		cparticipants << (CParticipant.new x, x.client.person)
-end
-
 
 
 #Printing
-=begin
-people.each{|x| puts x.export}
-clientFirms.each{|x| puts x.export}
-clientInd.each{|x| puts x.export}
-conferences.each{|x| puts x.export}
-workshops.each{|x| puts x.export}
-creservations.each{|x| puts x.export}
-wreservations.each{|x| puts x.export}
-=end
+#=begin
+
+peopleFile = File.new("export/Osoby.sql")
+clientsFile = File.new("export/Klienci.sql")
+conferencesFile = File.new("export/Konferencje.sql")
+workshopsFile = File.new("export/Warsztaty.sql")
+confReservationsFile = File.new("export/RezerwacjeKonf.sql")
+workReservationsFile = File.new("export/RezerwacjeWarsz.sql")
+confParticipantsFile = File.new("export/UczestnicyKonf.sql")
+workParticipantsFile = File.new("export/UczestnicyWarsz.sql")
+people.each{|x| peopleFile << x.export}
+clientFirms.each{|x| clientsFile << x.export}
+clientInd.each{|x| clientsFile << x.export}
+conferences.each{|x| conferencesFile << x.export}
+workshops.each{|x| workshopsFile << x.export}
+creservations.each{|x| confReservationsFile << x.export}
+wreservations.each{|x| workReservationsFile << x.export}
+cparticipants.each{|x| confParticipantsFile << x.export}
+wparticipants.each{|x| workParticipantsFile << x.export}
+#=end
 
 # Examples
 =begin
